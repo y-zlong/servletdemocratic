b1bc364a21c3d85283dd0eaf5866da224d8591b4
y-zlong <18710493547@163.com>
2018-05-12 14:22:00 +0800

    创建servletConfig对象

--- src/servletdemo/ServletConfigTest.java
+++ src/servletdemo/ServletConfigTest.java
@@ -3,10 +3,13 @@ package servletdemo;
  * 1.写一个类实现servlet接口
  * 2.重写servlet的方法
  * 3.在web.xml中配置servlet
+ * =========================
+ * servlet
  */
 import java.io.IOException;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -34,6 +37,9 @@ public class ServletConfigTest implements Servlet {
 	@Override
 	public void init(javax.servlet.ServletConfig arg0) throws ServletException {
 		System.out.println("init=============");
+		//获取servletConfig对象
+		//通过this关键字来获取servletConfig对象
+		ServletConfig config = this.getServletConfig();
 		
 	}
 
