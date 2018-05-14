77520b998e3e58530c3b9cb1ec1c4bc31b40d964
y-zlong <18710493547@163.com>
2018-05-14 10:25:24 +0800

    配置web应用初始化参数<context-param>

--- src/servletdemo/TestServlet.java
+++ src/servletdemo/TestServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Enumeration;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,6 +40,21 @@ public class TestServlet extends HttpServlet {
 			System.out.println(names1);
 			System.out.println(vules);
 		}
+		
+		//获取web应用初始化参数
+		ServletContext context = config.getServletContext();	//通过servletConfig对象获取ServletContext对象
+		String driver = context.getInitParameter("driver");			//调用getintParameter(String name)方法获取参数值
+			System.out.println(driver);
+		//迭代获取
+		Enumeration<String> name2 = context.getInitParameterNames();
+		//while循环
+		while(name2.hasMoreElements()) {
+			String Name = name2.nextElement();
+			System.out.println(Name);
+			String vules1 = context.getInitParameter(Name);
+			System.out.println(vules1);
+		}
+		
 	}
 
 	/**
